2e8f72aeda544420eee52e8f6e09aaaa03690449
Herman Rivas <hrivas5726@aol.com>
2019-06-05 11:43:32 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,21 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+class DigitalClicker extends React.Component {
+  state = {
+      timesClicked: 0
+    }
+
+  onClick = () => {
+    const incrementClick = this.state.timesClicked + 1
+    this.setState ({timesClicked: incrementClick})
+  }
+
+  render() {
+    return (
+      <button onClick={this.onClick}>{this.state.timesClicked}</button>
+    )
+  }
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,54 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+class YoutubeDebugger extends React.Component {
+    state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+
+
+  onClick = () => {
+    this.setState({
+        settings: {
+          ...this.state.settings,
+          bitrate: 12
+      }
+    }
+  )}
+
+  onResClick = () => {
+    this.setState({
+        settings: {
+          ...this.state.settings,
+          video: {
+            resolution: '720p'}
+          }
+    }
+  )}
+
+
+
+
+  render() {
+    return (
+      <div>
+        <button className='bitrate' onClick={this.onClick} ></button>
+        <button className='resolution' onClick={this.onResClick}></button>
+      </div>
+    )
+  }
+
+
+}
+
+
+
+
+export default YoutubeDebugger
